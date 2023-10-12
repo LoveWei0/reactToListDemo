62fa23cc07e299f080ad7961882cf8d012577265
LoveWei0 <xdw666888@qq.com>
2023-10-12 14:48:51 +0800

    feat: config path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// path->join
+import { join } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  // 路径别名
+  resolve: {
+    alias: {
+      '@': join(__dirname, 'src'),
+      '@store': join(__dirname, 'src/store'),
+      '@pages': join(__dirname, 'src/pages'),
+      '@app': join(__dirname, 'src/app')
+    }
+  }
 })
