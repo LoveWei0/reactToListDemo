8289ee32e851598b8e1f04d57f7ebdb5b56dd4a0
LoveWei0 <xdw666888@qq.com>
2023-10-13 10:50:44 +0800

    feat: add json data

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 // component
 // import TodoList from './pages/TodoList'
 // import ATodoList from './pages/ATodoList'
-import CoverShu from '@/pages/CoverShu'
+import CoverShu from '@/pages/jianshu/CoverShu'
 
 export default function App() {
   return (
