36bb9a3fdd220f0ad8acf98fcc0523b81d763611
lovewei0 <xdw666888@qq.com>
2023-10-14 13:50:58 +0800

    feat: fix config eslintrc.cjs and add model ReqHeaderList

--- src/api/headerList.ts
+++ src/api/headerList.ts
@@ -1,5 +1,6 @@
 import { http } from '@utils/index'
+import { ReqHeaderList } from './model/headerListType'
 
 export function getHeaderList() {
-  return http.get('/api/headerList')
+  return http.get<any, ReqHeaderList>('/api/headerList')
 }
---
+++ src/api/model/headerListType.ts
@@ -0,0 +1,3 @@
+export interface ReqHeaderList {
+  list: string[]
+}
